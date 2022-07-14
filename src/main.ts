834c3f2f2193588970661e153d69e3bd2ae79843
vFlicker <flickervladislav@gmail.com>
2022-07-14 21:39:36 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
+  const document = parse(config);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
